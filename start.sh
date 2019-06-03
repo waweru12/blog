37079ced28a126bd7df2e81d56aaa9a47c603bee
Santa Claus <santa@northpole.com>
2019-06-03 07:28:16 +0300

    created class blog

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='waweru12'
+export DATABASE_URL='postgresql+psycopg2://austin:1234localhost/blog'
+export MAIL_USERNAME='austinwaweru12@gmail.com'
+export MAIL_PASSWORD='1800waweru.'
+python3 manage.py server
